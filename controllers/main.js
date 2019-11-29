6a807fefe664459a33f95b1e83b37966c3af1a07
zakwarren <41547855+zakwarren@users.noreply.github.com>
2019-11-29 22:15:53 +0000

    display analysis

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,7 @@
+const Scraper = require('../tools/scraper');
+const WordAnalyzer = require('../tools/word-analyzer');
+
+
 exports.getHome = (req, res, next) => {
     res.render(
         'main/index',
@@ -9,11 +13,26 @@ exports.getHome = (req, res, next) => {
 };
 
 exports.postAnalysis = (req, res, next) => {
-    res.render(
-        'main/analysis',
-        {
-            pageTitle: 'Word Analysis',
-            path: '/analysis'
+    const url = req.body.url;
+    const urlSuffix = req.body.urlSuffix;
+
+    const analyzeWords = words => {
+        if (words) {
+            const analyze = new WordAnalyzer(words);
+            analyze.analyze();
+
+            res.render(
+                'main/analysis',
+                {
+                    pageTitle: 'Word Analysis',
+                    path: '/analysis',
+                    urlAnalyzed: url,
+                    analysis: analyze.analysis
+                }
+            );
         }
-    );
+    };
+    
+    const myScraper = new Scraper(url, urlSuffix);
+    myScraper.scrapeSite(analyzeWords);
 };
